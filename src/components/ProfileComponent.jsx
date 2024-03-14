24864c8fce3fc75742098a4f4e5d5bb212277dce
Chokkalingam-S <chok22017.it@rmkec.ac.in>
2024-03-14 23:21:18 +0530

    Minor changes

--- src/components/ProfileComponent.jsx
+++ src/components/ProfileComponent.jsx
@@ -1,20 +1,13 @@
 import React, { useState } from "react";
 import ProfileCard from "./common/ProfileCard";
 import ProfileEdit from "./common/ProfileEdit";
-
 export default function ProfileComponent({ currentUser }) {
   const [isEdit, setisEdit] = useState(false);
-
-  const onEdit = () => {
-    setisEdit(!isEdit);
-  };
+  const onEdit = () => {setisEdit(!isEdit);};
   return (
     <div>
-      {isEdit ? (
-        <ProfileEdit onEdit={onEdit} currentUser={currentUser} />
-      ) : (
-        <ProfileCard currentUser={currentUser} onEdit={onEdit} />
-      )}
+      {isEdit ? (<ProfileEdit onEdit={onEdit} currentUser={currentUser} />) 
+      : (<ProfileCard currentUser={currentUser} onEdit={onEdit} />)}
     </div>
   );
 }
--- src/components/RegisterComponent.jsx
+++ src/components/RegisterComponent.jsx
@@ -38,37 +38,15 @@ export default function RegisterComponent() {
       <div className="login-wrapper-inner">
       <h1 className="heading">Sign Up</h1>
         <p className="sub-heading">Make your professional life Better!</p>
-
-
         <div className="auth-inputs">
-          <input
-            onChange={(event) =>
-              setCredentials({ ...credentails, name: event.target.value })
-            }
-            type="text"
-            className="common-input"
-            placeholder="Your Name"
-          />
-          <input
-            onChange={(event) =>
-              setCredentials({ ...credentails, email: event.target.value })
-            }
-            type="email"
-            className="common-input"
-            placeholder="Email or phone number"
-          />
-          <input
-            onChange={(event) =>
-              setCredentials({ ...credentails, password: event.target.value })
-            }
-            type="password"
-            className="common-input"
-            placeholder="Password (6 or more characters)"
-          />
+          <input onChange={(event) =>setCredentials({ ...credentails, name: event.target.value })}
+            type="text" className="common-input" placeholder="Your Name"/>
+          <input onChange={(event) =>setCredentials({ ...credentails, email: event.target.value })}
+            type="email" className="common-input" placeholder="Email or phone number"/>
+          <input onChange={(event) =>setCredentials({ ...credentails, password: event.target.value })}
+            type="password" className="common-input" placeholder="Password (6 or more characters)" />
         </div>
-        <button onClick={register} className="login-btn">
-          Agree & Join
-        </button>
+        <button onClick={register} className="login-btn">Agree & Join</button>
       
       <hr class="hr-text" />
       <div className="google-btn-container">
