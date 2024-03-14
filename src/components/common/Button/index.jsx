6d1ca68c06599fe2b17d0cf481d91264575ee574
Chokkalingam-S <chok22017.it@rmkec.ac.in>
2024-03-14 23:17:57 +0530

    Button edit

--- src/components/common/Button/index.jsx
+++ src/components/common/Button/index.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import "./index.scss";
-
 export default function Button({ title, onClick }) {
-  return (
-    <button className="common-btn" onClick={onClick}>
-      {title}
-    </button>
-  );
+  return (<button className="common-btn" onClick={onClick}>{title} </button>);
 }
